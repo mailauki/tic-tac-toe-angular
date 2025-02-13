cbeb1d86731a41c740f10e01b36e574727fbfdac
Julie Evans <juliemevans17@gmail.com>
2025-02-13 15:28:47 -0600

    add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { BoardComponent } from "./board/board.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, BoardComponent],
+  imports: [RouterOutlet, MatButtonModule, BoardComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
