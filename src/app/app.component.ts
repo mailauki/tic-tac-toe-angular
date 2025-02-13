f029b65e1a00a14270b52db189a45ac935166244
Julie Evans <juliemevans17@gmail.com>
2025-02-13 15:38:10 -0600

    add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { BoardComponent } from "./board/board.component";
 import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, BoardComponent, HeaderComponent],
+  imports: [RouterOutlet, MatButtonModule, BoardComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
