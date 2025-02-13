d4c0b2bb0619374d27aef74d33d3670edfeb4879
Julie Evans <juliemevans17@gmail.com>
2025-02-13 17:17:21 -0600

    add update tile function

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatRippleModule} from '@angular/material/core';
+
+interface Tile {
+  value: string,
+  index: number,
+}
 
 @Component({
   selector: 'app-board',
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, MatRippleModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
-  grid = Array.from(Array(9)).map((_, index) => index)
+  grid: Tile[] = Array.from(Array(9)).map((_, index) => Object.assign({ value: " ", index: index }))
+
+  handleAddToken(index: number, newValue: string) {
+    console.log(index)
+    this.grid = this.grid.map((tile) => {
+      if (tile.index === index) {
+        return { ...tile, value: newValue };
+      }
+      return tile;
+    });
+  }
 }
